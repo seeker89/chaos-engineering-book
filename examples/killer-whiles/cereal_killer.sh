491f23670a74d8d87134bfe6febd9b8a3ee3836b
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2019-12-28 20:47:51 +0000

    Remove too much sleeping

--- examples/killer-whiles/cereal_killer.sh
+++ examples/killer-whiles/cereal_killer.sh
@@ -7,14 +7,12 @@ echo "Killing instance A (port 8001)"
 ps auxf | grep killer-whiles | grep python | grep 8001 | awk '{system("sudo kill " $2)}'
 sleep 1
 systemctl status faas001_a
-sleep 1
 echo
 
 echo "Killing instance A (port 8002)"
 ps auxf | grep killer-whiles | grep python | grep 8002 | awk '{system("sudo kill " $2)}'
 sleep 1
 systemctl status faas001_b
-sleep 1
 echo
 
 echo "Listing backend services"
