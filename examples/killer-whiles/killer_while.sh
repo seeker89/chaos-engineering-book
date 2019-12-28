5d3039b87cf5c881afe4453ba77ccf0b3c123a88
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2019-12-28 22:22:49 +0000

    There are only three problems in computer science: naming things, and one-off errors

--- examples/killer-whiles/killer_while.sh
+++ examples/killer-whiles/killer_while.sh
@@ -1,6 +1,6 @@
 # restart instance A 5 times, spaced out by 1.5 second delays
 i="0"
-while [ $i -lt 4 ]
+while [ $i -le 4 ]
 do
     echo "Restarting faas001_a ${i}th time"
     sudo systemctl restart faas001_a
@@ -10,7 +10,7 @@ done
 
 # restart instance B 5 times, spaced out by 1.5 second delays
 i="0"
-while [ $i -lt 4 ]
+while [ $i -le 4 ]
 do
     echo "Restarting faas001_b ${i}th time"
     sudo systemctl restart faas001_b
