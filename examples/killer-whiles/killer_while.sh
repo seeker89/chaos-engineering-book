344f7910d1a19cca7ec1a7d3236e9f4425aa1592
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2019-12-28 22:59:32 +0000

    Tweak the timeouts

--- examples/killer-whiles/killer_while.sh
+++ examples/killer-whiles/killer_while.sh
@@ -4,11 +4,13 @@ while [ $i -le 5 ]
 do
     echo "Killing faas001_a ${i}th time"
     ps auxf | grep killer-whiles | grep python | grep 8001 | awk '{system("sudo kill " $2)}'
-    sleep 1.25
+    sleep 0.25
     i=$[$i+1]
 done
 
 systemctl status faas001_a --no-pager
+
+sleep 2
 
 # restart instance B a few times, spaced out by 1.5 second delays
 i="0"
@@ -16,7 +18,7 @@ while [ $i -le 5 ]
 do
     echo "Killing faas001_b ${i}th time"
     ps auxf | grep killer-whiles | grep python | grep 8002 | awk '{system("sudo kill " $2)}'
-    sleep 1.25
+    sleep 0.25
     i=$[$i+1]
 done
 
