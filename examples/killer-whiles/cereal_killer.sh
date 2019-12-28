519d174143c87153e3d2774cade49a48f6582349
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2019-12-28 20:30:25 +0000

    Some extra whitespace

--- examples/killer-whiles/cereal_killer.sh
+++ examples/killer-whiles/cereal_killer.sh
@@ -1,18 +1,21 @@
 echo "Listing backend services"
 ps auxf | grep killer-whiles | grep python
 sleep 1
+echo
 
 echo "Killing instance A (port 8001)"
 ps auxf | grep killer-whiles | grep python | grep 8001 | awk '{system("sudo kill " $2)}'
 sleep 1
 systemctl status faas001_a
 sleep 1
+echo
 
 echo "Killing instance A (port 8002)"
 ps auxf | grep killer-whiles | grep python | grep 8002 | awk '{system("sudo kill " $2)}'
 sleep 1
 systemctl status faas001_b
 sleep 1
+echo
 
 echo "Listing backend services"
 ps auxf | grep killer-whiles | grep python
