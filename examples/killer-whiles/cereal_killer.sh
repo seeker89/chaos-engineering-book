7c458e142321c92e2cd0234c24aafefe3cf07b09
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2019-12-28 20:49:58 +0000

    Give it 1.5 seconds to recover

--- examples/killer-whiles/cereal_killer.sh
+++ examples/killer-whiles/cereal_killer.sh
@@ -1,17 +1,16 @@
 echo "Listing backend services"
 ps auxf | grep killer-whiles | grep python
-sleep 1
 echo
 
 echo "Killing instance A (port 8001)"
 ps auxf | grep killer-whiles | grep python | grep 8001 | awk '{system("sudo kill " $2)}'
-sleep 1
+sleep 1.5
 systemctl status faas001_a
 echo
 
 echo "Killing instance A (port 8002)"
 ps auxf | grep killer-whiles | grep python | grep 8002 | awk '{system("sudo kill " $2)}'
-sleep 1
+sleep 1.5
 systemctl status faas001_b
 echo
 
