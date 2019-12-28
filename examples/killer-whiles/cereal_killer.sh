9afeccb80ce548ecbfe366eebd25899deb6e32bb
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2019-12-28 21:04:48 +0000

    Sleep 2 seconds between killings

--- examples/killer-whiles/cereal_killer.sh
+++ examples/killer-whiles/cereal_killer.sh
@@ -4,13 +4,17 @@ echo
 
 echo "Killing instance A (port 8001)"
 ps auxf | grep killer-whiles | grep python | grep 8001 | awk '{system("sudo kill " $2)}'
-sleep 1.5
+sleep 0.5
 systemctl status faas001_a
+echo
+
+echo "Wait some time in-between killings"
+sleep 2
 echo
 
 echo "Killing instance A (port 8002)"
 ps auxf | grep killer-whiles | grep python | grep 8002 | awk '{system("sudo kill " $2)}'
-sleep 1.5
+sleep 0.5
 systemctl status faas001_b
 echo
 
