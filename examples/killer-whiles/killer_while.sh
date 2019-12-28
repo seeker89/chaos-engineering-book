3223da5724b5f4a099881e7cf269e72a6ad17ff7
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2019-12-28 22:20:06 +0000

    Throw in sudo to the killer while

--- examples/killer-whiles/killer_while.sh
+++ examples/killer-whiles/killer_while.sh
@@ -3,7 +3,7 @@ i="0"
 while [ $i -lt 4 ]
 do
     echo "Restarting faas001_a $ith time"
-    systemctl restart faas001_a
+    sudo systemctl restart faas001_a
     sleep 1.5
     i=$[$i+1]
 done
@@ -13,7 +13,7 @@ i="0"
 while [ $i -lt 4 ]
 do
     echo "Restarting faas001_b $ith time"
-    systemctl restart faas001_b
+    sudo systemctl restart faas001_b
     sleep 1.5
     i=$[$i+1]
 done
