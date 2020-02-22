0e604efbdca487fda06de1f85ee9274618f22af1
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-02-22 23:28:42 +0000

    Connect to mysql on 127.0.0.1 for wordpress, not localhost

--- vm/ansible/prerequisites/templates/config-localhost.php
+++ vm/ansible/prerequisites/templates/config-localhost.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_NAME', 'wordpress');
 define('DB_USER', 'wordpress');
-define('DB_PASSWORD', 'wordpress');
+define('DB_PASSWORD', '127.0.0.1');
 define('DB_HOST', 'localhost');
 define('WP_CONTENT_DIR', '/usr/share/wordpress/wp-content');
 define('WP_DEBUG', true);
