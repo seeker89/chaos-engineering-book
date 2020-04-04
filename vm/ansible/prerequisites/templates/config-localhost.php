ac7bebd1f1a273eb4f307e82e3fd5ed34654be2f
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-04-04 18:04:52 +0100

    Fix wordpress settings :trollface: changed the wrong field with the wrong value :)

--- vm/ansible/prerequisites/templates/config-localhost.php
+++ vm/ansible/prerequisites/templates/config-localhost.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_NAME', 'wordpress');
 define('DB_USER', 'wordpress');
-define('DB_PASSWORD', '127.0.0.1');
-define('DB_HOST', 'localhost');
+define('DB_PASSWORD', 'wordpress');
+define('DB_HOST', '127.0.0.1');
 define('WP_CONTENT_DIR', '/usr/share/wordpress/wp-content');
 define('WP_DEBUG', true);
 //define('WP_DEBUG_LOG', true);
