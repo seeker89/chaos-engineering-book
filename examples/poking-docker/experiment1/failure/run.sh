6e391d02c26b3ec0edc906f9770c079800ae1fa4
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-04-26 17:02:44 +0100

    Don't sleep

--- examples/poking-docker/experiment1/failure/run.sh
+++ examples/poking-docker/experiment1/failure/run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-FILESIZE=$((100*1024*1024))
+FILESIZE=$((50*1024*1024))
 FILENAME=testfile
 echo "Press [CTRL+C] to stop.."
 count=0
@@ -7,6 +7,5 @@ while :
 do
     new_name=$FILENAME.$count
     fallocate -l $FILESIZE $new_name && echo "OK wrote the file" `ls -alhi $new_name` || echo "Couldn't write the file" $new_name
-    sleep 1
     (( count++ ))
 done
