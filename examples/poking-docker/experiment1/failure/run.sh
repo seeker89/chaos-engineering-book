69dea797fc67c5fd6a19b799aa2a4ebdc15fb2b9
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-04-26 17:06:26 +0100

    Sleep a little bit

--- examples/poking-docker/experiment1/failure/run.sh
+++ examples/poking-docker/experiment1/failure/run.sh
@@ -6,6 +6,6 @@ count=0
 while :
 do
     new_name=$FILENAME.$count
-    fallocate -l $FILESIZE $new_name && echo "OK wrote the file" `ls -alhi $new_name` || echo "Couldn't write the file" $new_name
+    fallocate -l $FILESIZE $new_name && echo "OK wrote the file" `ls -alhi $new_name` || echo "Couldn't write the file" $new_name "Sleeping a bit"; sleep 1
     (( count++ ))
 done
