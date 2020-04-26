b6746c9732bcd0d1b92657b27c5b5a574b70bc1a
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-04-26 16:57:25 +0100

    Space vs tabs

--- examples/poking-docker/experiment1/control/run.sh
+++ examples/poking-docker/experiment1/control/run.sh
@@ -5,6 +5,6 @@ echo "Press [CTRL+C] to stop.."
 while :
 do
     fallocate -l $FILESIZE $FILENAME && echo "OK wrote the file" `ls -alhi $FILENAME` || echo "Couldn't write the file"
-	sleep 2
+    sleep 2
     rm $FILENAME || echo "Couldn't delete the file"
 done
