38db097d9ac4ed544136b98ed3957f3ded8a3ca0
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-04-26 16:41:40 +0100

    Don't print the initial info all the time

--- examples/poking-docker/experiment1/control/run.sh
+++ examples/poking-docker/experiment1/control/run.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 FILESIZE=$((50*1024*1024))
 FILENAME=testfile
+echo "Press [CTRL+C] to stop.."
 while :
 do
-	echo "Press [CTRL+C] to stop.."
     fallocate -l $FILESIZE $FILENAME && echo "Wrote the file" || "Couldn't write the file"
 	sleep 2
 done
