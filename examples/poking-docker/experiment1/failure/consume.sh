57912898ff00f8dc48c970f979812e53e399ca27
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-04-28 09:54:14 +0100

    Increase readability

--- examples/poking-docker/experiment1/failure/consume.sh
+++ examples/poking-docker/experiment1/failure/consume.sh
@@ -6,6 +6,8 @@ count=0
 while :
 do
     new_name=$FILENAME.$count
-    fallocate -l $FILESIZE $new_name && echo "OK wrote the file" `ls -alhi $new_name` || (echo "Couldn't write the file" $new_name "Sleeping a bit"; sleep 5)
+    fallocate -l $FILESIZE $new_name \
+        && echo "OK wrote the file" `ls -alhi $new_name` \
+        || (echo "Couldn't write the file" $new_name "Sleeping a bit"; sleep 5)
     (( count++ ))
 done
