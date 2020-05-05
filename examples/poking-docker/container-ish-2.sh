0aab1406e57cbf532863654e690ece272f94d1fe
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-05-05 09:38:12 +0100

    Better printing

--- examples/poking-docker/container-ish-2.sh
+++ examples/poking-docker/container-ish-2.sh
@@ -7,7 +7,7 @@ RAM_LIMIT=${2:-5242880}
 echo "Step A: generate a unique ID (uuid)"
 UUID=$(date | sha256sum | cut -f1 -d" ")
 
-echo "Step B: create cgroups"
+echo "Step B: create cpu and memory cgroups"
 sudo mkdir /sys/fs/cgroup/{cpu,memory}/$UUID
 echo $RAM_LIMIT | sudo tee /sys/fs/cgroup/memory/$UUID/memory.limit_in_bytes
 echo 100000 | sudo tee /sys/fs/cgroup/cpu/$UUID/cpu.cfs_period_us
@@ -16,7 +16,7 @@ echo $CPU_LIMIT | sudo tee /sys/fs/cgroup/cpu/$UUID/cpu.cfs_quota_us
 echo "Step C: prepare the folder structure to be our chroot"
 bash $CURRENT_DIRECTORY/new-filesystem.sh $UUID > /dev/null && cd $UUID
 
-echo "Step D: put the current process into the cgroups"
+echo "Step D: put the current process (PID $$) into the cgroups"
 echo $$ | sudo tee /sys/fs/cgroup/{cpu,memory}/$UUID/tasks
 
 echo "Step E: start our namespaced chroot container-ish: $UUID"
