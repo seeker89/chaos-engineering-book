c4b45f24adb6db264a772b7579fbaea3f91a764b
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-05-13 14:12:59 +0100

    Update to use abandonware

--- examples/who-you-gonna-call/src/legacy/writer.c
+++ examples/who-you-gonna-call/src/legacy/writer.c
@@ -1,10 +1,10 @@
 
 #include <unistd.h>
-#include "agile_0.h"
+#include "abandonware_0.h"
 
 ssize_t write_content(int fd){
     // Not sure what else should go here, but here's for nothing
-    agile_0(fd);
+    abandonware_0(fd);
     // TODO return something useful
     // TODO error handling
     return 0;
