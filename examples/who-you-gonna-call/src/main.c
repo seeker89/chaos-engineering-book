842bac8fa6ed2bbd95620878a1e5e2ba115051df
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-05-15 13:22:35 +0100

    New line

--- examples/who-you-gonna-call/src/main.c
+++ examples/who-you-gonna-call/src/main.c
@@ -57,7 +57,7 @@ int main()
     int arbitrary_queue_len = 1000;
     listen(sock, arbitrary_queue_len);
 
-    printf("Listening on port %d", PORT);
+    printf("Listening on port %d\n", PORT);
 
     // accept some connection
     int client_fd;
