a5208b354b876508a3d5e89d916787be6ac1579e
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-05-15 11:25:27 +0100

    long address

--- examples/who-you-gonna-call/src/main.c
+++ examples/who-you-gonna-call/src/main.c
@@ -32,7 +32,7 @@ int main()
     // build the struct
     struct sockaddr_in address;
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
     address.sin_port = htons(8080);
 
     // bind and listen
