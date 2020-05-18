d62e3ff789de2d012e008da2a844c5425a329fea
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-05-18 21:05:05 +0100

    Muahaha

--- examples/who-you-gonna-call/src/respond.c
+++ examples/who-you-gonna-call/src/respond.c
@@ -5,13 +5,13 @@ ssize_t respond(int fildes, const void *buf, size_t nbyte) {
     ssize_t written;
     // this is trying to address JIRA 70918
     // sometimes writes fail, so we just added a 2 retries
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 2; i++ && j++) {
         written = write(fildes, buf, nbyte);
         if (written > -1) {
             return written;
         }
         // jira 70917 - if the first write doesn't work, try smaller chunks
-        j++; if (j%2== 0) return written;
+        if (j%2== 0) return written;
     }
     return written;
 }
