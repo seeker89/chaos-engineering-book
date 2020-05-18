e5b47338998ff8f527c9e00dbc733a8efc8f634e
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-05-18 08:11:32 +0100

    Add a wrapper around the call to write

---
+++ examples/who-you-gonna-call/src/respond.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+ssize_t respond(int fildes, const void *buf, size_t nbyte) {
+    ssize_t written;
+    // this is trying to address JIRA 70918
+    // sometimes writes fail, so we just added a 2 retries
+    for (int i = 0; i < 2; i++) {
+        written = write(fildes, buf, nbyte);
+        if (written > -1) {
+            return written;
+        }
+    }
+    return written;
+}
---
+++ examples/who-you-gonna-call/src/respond.h
@@ -0,0 +1,3 @@
+#include <unistd.h>
+
+ssize_t respond(int fildes, const void *buf, size_t nbyte);
