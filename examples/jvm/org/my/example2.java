d2e87f22c2cbd73c4d6807c92f017091295c6cf2
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-06-03 11:55:25 +0100

    Add example2 throwing an IOException

---
+++ examples/jvm/org/my/example2.java
@@ -0,0 +1,10 @@
+package org.my;
+import java.io.IOException;
+
+class Example2
+{
+    public static void main(String[] args) throws IOException
+    {
+        throw new IOException("Oops");
+    }
+}
