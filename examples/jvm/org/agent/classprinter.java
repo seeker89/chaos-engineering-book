6e994c4fcae434147f82f46549f083cb0025a4ee
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-06-05 10:43:48 +0100

    Add a very simple agent1.jar

---
+++ examples/jvm/org/agent/classprinter.java
@@ -0,0 +1,12 @@
+package org.agent;
+
+import java.lang.instrument.ClassFileTransformer;
+
+class ClassPrinter implements ClassFileTransformer {
+    public byte[] transform(ClassLoader loader,
+                            String className,
+                            Class<?> classBeingRedefined,
+                            ProtectionDomain protectionDomain,
+                            byte[] classfileBuffer) throws IllegalClassFormatException {
+    System.out.println("Found class: " + className + " (" + classfileBuffer.length + " bytes)");
+  }
