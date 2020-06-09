c2dbb09f9f84d51e9ed2dffb0a53f963bf755284
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-06-09 12:41:15 +0100

    Update example2 to call static method

--- examples/jvm/org/my/example2.java
+++ examples/jvm/org/my/example2.java
@@ -5,6 +5,11 @@ class Example2
 {
     public static void main(String[] args) throws IOException
     {
+        Example2.throwIOException();
+    }
+
+    public static void throwIOException() throws IOException
+    {
         throw new IOException("Oops");
     }
 }
