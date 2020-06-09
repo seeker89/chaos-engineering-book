30b1bc07d00ef5e2416bdea78d51e722f409e7cc
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-06-09 12:47:18 +0100

    Attempt to inject a static method invocation

--- examples/jvm/org/agent2/ClassInjector.java
+++ examples/jvm/org/agent2/ClassInjector.java
@@ -4,6 +4,10 @@ import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
+import jdk.internal.org.objectweb.asm.ClassReader;
+import jdk.internal.org.objectweb.asm.ClassWriter;
+import jdk.internal.org.objectweb.asm.tree.*;
+
 
 class ClassInjector implements ClassFileTransformer {
 
@@ -16,7 +20,35 @@ class ClassInjector implements ClassFileTransformer {
                             byte[] classfileBuffer) throws IllegalClassFormatException {
     if (className.equals(this.targetClassName)){
       System.out.println("TARGET ACQUIRED: " + className + " (" + classfileBuffer.length + " bytes)");
+
+      ClassNode classNode = new ClassNode();
+      new ClassReader(classFileBuffer).accept(classNode, 0);
+      classNode.methods.stream()
+        .filter(method -> method.name.equals("output"))
+        .forEach(method -> {
+
+          InsnList instructions = new InsnList();
+          instructions.add(new MethodInsnNode(
+              Opcodes.INVOKESTATIC,
+              "org/agent2/ClassInjector",
+              "output",
+              "()Ljava/io/IOException;"
+          ));
+          method.maxStack += 1;
+          method.instructions.insertBefore(
+              method.instructions.getFirst(),
+              instructions
+          );
+        });
+        final ClassWriter classWriter = new ClassWriter(0);
+        classNode.accept(classWriter);
+        return classWriter.toByteArray();
     }
     return classfileBuffer;
+  }
+
+  public static void throwIOException() throws IOException
+  {
+      throw new IOException();
   }
 }
