2cfe0987aaa777a9cf834861cf611a9dea309a2c
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-06-10 11:42:41 +0100

    Delete wrapper.sh

--- examples/jvm/wrapper.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo "Our pid is" $$
-echo "Press enter to start FizzBuzzEnterprise"
-read
-
-exec java -classpath "./FizzBuzzEnterpriseEdition/lib/*" com.seriouscompany.business.java.fizzbuzz.packagenamingpackage.impl.Main
