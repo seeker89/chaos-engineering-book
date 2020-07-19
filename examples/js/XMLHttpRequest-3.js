b5d6d8f62a2e02f44da438017ca06d7040f4014c
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-07-19 12:54:26 +0100

    A little message

--- examples/js/XMLHttpRequest-3.js
+++ examples/js/XMLHttpRequest-3.js
@@ -1,9 +1,8 @@
-const originalMethod = window.XMLHttpRequest.prototype.send;
+const originalSend = window.XMLHttpRequest.prototype.send;
 window.XMLHttpRequest.prototype.send = function(){
+    console.log("Chaos calling", new Date());
     let that = this;
-    let args = [...arguments];
-    let cb = function() {
-        return originalMethod.apply(that, args);
-    }
-    setTimeout(cb, 1000);
+    setTimeout(function() {
+        return originalSend.apply(that, [...arguments]);
+    }, 1000);
 }
