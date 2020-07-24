9702ecdd83580319604e0f0aa5a054affb96515b
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2020-07-24 09:35:53 +0100

    Be consistent with the other stuff

--- examples/killer-whiles/killer_while.sh
+++ examples/killer-whiles/killer_while.sh
@@ -6,7 +6,7 @@ while [ $i -le 5 ]
 do
     echo "Killing faas001_a ${i}th time"
     ps auxf | grep killer-whiles | grep python | grep 8001 | awk '{system("sudo kill " $2)}'
-    sleep 0.5
+    sleep 1.25
     i=$[$i+1]
 done
 
@@ -20,7 +20,7 @@ while [ $i -le 5 ]
 do
     echo "Killing faas001_b ${i}th time"
     ps auxf | grep killer-whiles | grep python | grep 8002 | awk '{system("sudo kill " $2)}'
-    sleep 0.5
+    sleep 1.25
     i=$[$i+1]
 done
 
